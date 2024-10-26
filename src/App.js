1c672b751c3afffd1d40e4784d5861c134a1d368
dev-anu <anu.infy850@gmail.com>
2024-10-27 02:47:11 +0530

    fixed issue in deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Anurag Tripathi
+          Anurag Tripathi, Deployed
         </a>
       </header>
     </div>
